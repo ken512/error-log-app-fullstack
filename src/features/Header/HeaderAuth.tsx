ccdd4e9129c51f6be0b9d8efef3d8627e664d57a
ken512 <ken887265@gmail.com>
2026-08-15 23:02:54 +0900

    feat: ヘッダーコンポーネント基盤とセッション取得と認証切り替えUIの作成

---
+++ src/features/Header/HeaderAuth.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { auth } from "@/utils/auth/auth";
+import { logout } from "../auth/actions/signout";
+
+// セッション取得と認証UI切り替えのコンポーネント
+export const HeaderAuth = async () => {
+  const session = await auth();
+
+  return (
+    <>
+      <Link href="/">error_log</Link>
+      {session?.user ? (
+        <div className="flex items-center gap-4">
+          <span className="text-white text-md hover:text-gray-300">
+            ログイン中
+          </span>
+
+          <form action={logout}>
+            <button
+              type="submit"
+              className="rounded bg-white px-4 py-2 text-black hover:bg-gray-400"
+            >
+              ログアウト
+            </button>
+          </form>
+        </div>
+      ) : (
+        <div className="flex items-center gap-4">
+          <Link
+            href="/login"
+            className="text-white text-md hover:text-gray-300 "
+          >
+            ログイン
+          </Link>
+          <Link
+            href="/signup"
+            className="text-white text-md hover:text-gray-300"
+          >
+            サインアップ
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
---
+++ src/features/Header/index.tsx
@@ -0,0 +1,11 @@
+import { HeaderAuth } from "./HeaderAuth";
+
+export const Header = () => {
+  
+
+  return (
+    <header className="flex justify-between mx-10">
+      <HeaderAuth/>
+    </header>
+  );
+};
