5ab5b80b82fc5a8431bcef9416dcd71d16b51706
ken512 <ken887265@gmail.com>
2026-09-07 23:37:49 +0900

    feat: AxiosとTanStack Queryによる共通API通信処理を追加

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,9 +1,32 @@
 import { api } from "@/utils/api";
 
 // APIへリクエストをする通常の非同期関数
-// GETリクエストを実行して、レスポンスのデータを返す通常の非同期関数
-export const fetcher = async <T>(path: string): Promise<T> => {
-  const res = await api.get<T>(path);
+
+// GET: データを取得
+export const getFetcher = async <TResponse>(path: string): Promise<TResponse> => {
+  const res = await api.get<TResponse>(path);
 
   return res.data;
 };
+
+// POST: 登録データを送り、レスポンスを受け取る
+export const postFetcher = async <TRequest,TResponse>(path: string, data: TRequest): Promise<TResponse> => {
+  const res = await api.post<TResponse>(path, data);
+
+  return res.data;
+};
+
+
+// PUT: 更新データを送り、レスポンスで受け取る
+export const putFetcher = async <TRequest, TResponse>(path: string, data: TRequest): Promise<TResponse> => {
+  const res = await api.put<TResponse>(path, data);
+
+  return res.data;
+};
+
+// DELETE: URLで削除対象を指定し、レスポンスで受け取
+export const deleteFetcher = async <TResponse>(path: string): Promise<TResponse> => {
+  const res = await api.delete<TResponse>(path);
+
+  return res.data;
+}
